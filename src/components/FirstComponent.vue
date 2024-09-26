11f0e71e86f0bc6109710abbc4c1bd67376a9f20
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-09-27 01:13:04 +0530

    Created First Component 🚀

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent',
+  data() {
+    return {
+      message: 'My First Component In Vue.js!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
