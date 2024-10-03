26f5bc32cb44220d53c9cbff5d3e2bb6e54d7fe2
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-10-03 23:53:37 +0530

    How to Get Checkbox and Radio Button Value in Vue.js

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>
-    <h1>Conditional Rendering</h1>
-    <button @click="toggle">Toggle Visibility</button>
-    <p v-if="isVisible">Visible with v-if</p>
-    <p v-show="isVisible">Visible with v-show</p>
+  <div class="main">
+    <h1>Get Values From Check Box And Radio Buttons ...</h1>
+    <div class="input-container">
+      <h1>Technology</h1>
+      <div class="input-section">
+        <label for="PHP">PHP</label>
+        <input type="checkbox" value="PHP" v-model="technology" id="PHP" />
+        <label for="Wordpress">Wordpress</label>
+        <input type="checkbox" value="Wordpress" v-model="technology" id="Wordpress" />
+        <label for="Laravel">Laravel</label>
+        <input type="checkbox" value="Laravel" v-model="technology" id="Laravel" />
+        <h2>
+          {{ technology }}
+        </h2>
+      </div>
+      <h1>Profession</h1>
+      <div class="input-section">
+        <label for="Student">Student</label>
+        <input type="radio" v-model="profession" value="Student" id="Student" />
+        <label for="Developer">Developer</label>
+        <input type="radio" v-model="profession" value="Developer" id="Developer" />
+        <h2>
+          {{ profession }}
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,20 +32,36 @@
 export default {
   data() {
     return {
-      isVisible: true
-    }
-  },
-  methods: {
-    toggle() {
-      this.isVisible = !this.isVisible;
+      technology: [],
+      profession: null
     }
   }
 }
 </script>
 
-
 <style scoped>
 h1 {
   color: #42b983;
+}
+
+h2 {
+  color: chartreuse;
+}
+
+.main {
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+}
+
+.input-container {
+  margin: 0px 20px;
+  align-items: center;
+  font-size: large;
+}
+
+.input-section {
+  display: flex;
+  gap: 10px;
 }
 </style>
