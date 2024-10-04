beede7c1a6cd43c686c479bb0693d904899e6181
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-10-04 23:37:01 +0530

    Mastering Computed Properties in Vue.js

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,30 +1,6 @@
 <template>
-  <div class="main">
-    <h1>Get Values From Check Box And Radio Buttons ...</h1>
-    <div class="input-container">
-      <h1>Technology</h1>
-      <div class="input-section">
-        <label for="PHP">PHP</label>
-        <input type="checkbox" value="PHP" v-model="technology" id="PHP" />
-        <label for="Wordpress">Wordpress</label>
-        <input type="checkbox" value="Wordpress" v-model="technology" id="Wordpress" />
-        <label for="Laravel">Laravel</label>
-        <input type="checkbox" value="Laravel" v-model="technology" id="Laravel" />
-        <h2>
-          {{ technology }}
-        </h2>
-      </div>
-      <h1>Profession</h1>
-      <div class="input-section">
-        <label for="Student">Student</label>
-        <input type="radio" v-model="profession" value="Student" id="Student" />
-        <label for="Developer">Developer</label>
-        <input type="radio" v-model="profession" value="Developer" id="Developer" />
-        <h2>
-          {{ profession }}
-        </h2>
-      </div>
-    </div>
+  <div class="main-container">
+    <h1>Cart Total: {{ totalPrice }}</h1>
   </div>
 </template>
 
@@ -32,8 +8,15 @@
 export default {
   data() {
     return {
-      technology: [],
-      profession: null
+      items: [
+        { name: 'Apple', price: 2 },
+        { name: 'Banana', price: 1 }
+      ]
+    }
+  },
+  computed: {
+    totalPrice() {
+      return this.items.reduce((sum, item) => sum + item.price, 0)
     }
   }
 }
@@ -44,24 +27,10 @@ h1 {
   color: #42b983;
 }
 
-h2 {
-  color: chartreuse;
-}
-
-.main {
+.main-container {
+  display: flex;
+  justify-content: center;
   min-height: 80vh;
-  display: flex;
   align-items: center;
-}
-
-.input-container {
-  margin: 0px 20px;
-  align-items: center;
-  font-size: large;
-}
-
-.input-section {
-  display: flex;
-  gap: 10px;
 }
 </style>
