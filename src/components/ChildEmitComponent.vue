84fd97a00239b05055aafc2a34cc99880185561a
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-10-16 23:56:05 +0530

    Emitting Events from Child to Parent Components in Vue.js

---
+++ src/components/ChildEmitComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h2>Child Component</h2>
+    <button @click="sendAlert">Click to Emit Event</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildEmitComponent',
+  methods: {
+    sendAlert() {
+      this.$emit('alert-parent', 'Hello from Child!')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  padding: 20px;
+}
+button {
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #358a63;
+}
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Parent Component</h1>
+    <ChildEmitComponent @alert-parent="handleAlert" />
+  </div>
+</template>
+
+<script>
+import ChildEmitComponent from './ChildEmitComponent.vue'
+
+export default {
+  name: 'ParentComponent',
+  components: {
+    ChildEmitComponent
+  },
+  methods: {
+    handleAlert(message) {
+      alert(message)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding-top: 100px;
+  text-align: center;
+}
+</style>
